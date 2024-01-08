6e854185b13ad5385076aca899cfba9d341a30cc
Tesean Ferguson <tferguson1028@outlook.com>
2024-01-08 11:47:01 -0500

    Added routes to pages

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import logo from './logo.svg';
 import './App.css';
 import { useState } from 'react';
 
+import { Route, Routes } from 'react-router-dom';
+
 // Components
 import AuthPage from './components/AuthPage';
 import NewOrderPage from './components/NewOrderPage';
@@ -12,7 +14,14 @@ function App()
   const [ user, setUser ] = useState(null);
   return (
     <div className="App">
-      { user ? <NewOrderPage /> : <AuthPage /> }
+      { 
+        user ? 
+        <Routes>
+          <Route path={`/orders`} element={<OrderHistoryPage />} />
+          <Route path={`/orders/new`} element={<NewOrderPage />} />
+        </Routes> : 
+        <AuthPage /> 
+      }
     </div>
   );
 }
