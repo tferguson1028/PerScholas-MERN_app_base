2d3e97779f649b93fb4caf35f04bf37946f6a622
Tesean Ferguson <tferguson1028@outlook.com>
2024-01-09 11:20:44 -0500

    Did something useless

---
+++ crud-helper.js
@@ -0,0 +1,8 @@
+const User = require('./models/user');
+// const Item = require('./models/item');
+// const Category = require('./models/category');
+// const Order = require('./models/order');
+
+// Local variables will come in handy
+let user, item, category, order;
+let users, items, categories, orders;
