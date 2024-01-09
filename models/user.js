8e95da6799f44f10a8cd22516f1bf5b585782526
Tesean Ferguson <tferguson1028@outlook.com>
2024-01-09 11:11:48 -0500

    Added pre-hash to user schema

--- models/user.js
+++ models/user.js
@@ -31,4 +31,14 @@ const userSchema = new Schema(
   }
 });
 
+userSchema.pre('save', async function(next) 
+{
+  // 'this' is the user doc
+  if (!this.isModified('password')) return next();
+  
+  // update the password with the computed hash
+  this.password = await bcrypt.hash(this.password, SALT_ROUNDS);
+  return next();
+});
+
 module.exports = mongoose.model('User', userSchema);
