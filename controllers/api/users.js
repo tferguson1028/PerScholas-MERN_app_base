92f82b1556ac460ddda60c6244a017481641152f
Tesean Ferguson <tferguson1028@outlook.com>
2024-01-09 11:31:00 -0500

    Added and implemented jsonwebtoken

--- controllers/api/users.js
+++ controllers/api/users.js
@@ -1,14 +1,42 @@
-function create(req, res)
+const User = require("../../models/user.js")
+const jwt = require("jsonwebtoken");
+
+async function create(req, res)
 {
-  // Baby step...
-  res.json(
+  try
   {
-    user: 
-    {
-      name: req.body.name,
-      email: req.body.email
-    }
-  });
+    // Add user to db
+    const user = await User.create(req.body);
+    const token = createJWT(user);
+    res.json(token);
+  }catch(exception)
+  {
+    res.status(400).json(exception)
+  }
+}
+
+function createJWT(user) 
+{
+  // Returns String
+  return jwt.sign(
+    // data payload
+    { user },
+    process.env.SECRET,
+    { expiresIn: '24h' }
+  );
 }
 
 module.exports = { create };
+
+
+// function createExample(req, res)
+// {
+//   res.json(
+//   {
+//     user: 
+//     {
+//       name: req.body.name,
+//       email: req.body.email
+//     }
+//   });
+// }
