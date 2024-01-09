5072dbfbd78e5018c42784801e6e3e3131af09b8
Tesean Ferguson <tferguson1028@outlook.com>
2024-01-09 14:05:23 -0500

    Added functions to users-service.js

--- src/utilities/users-service.js
+++ src/utilities/users-service.js
@@ -8,10 +8,31 @@ export async function signUp(userData)
   // which will ultimately return a JSON Web Token (JWT)
   const token = await usersAPI.signUp(userData);
   
-  localStorage.setItem('token', token);
-  
-  // Baby step by returning whatever is sent back by the server
+  localStorage.setItem("token", token);
   return token;
 }
 
+export function getToken()
+{
+  const token = localStorage.getItem("token");
+  
+  if(!token) return null;
+  
+  const payload = JSON.parse(atob(token.split(".")[1]));
+  if(payload.exp < Date.now() / 1000) // Expired token
+  {
+    localStorage.removeItem("token");
+    return null;
+  }
+  
+  return token;
+}
+
+export function getUser()
+{
+  const token = getToken();
+  
+  // If there's a token, return the user in the payload, otherwise return null
+  return token ? JSON.parse(atob(token.split('.')[1])).user : null;
+}
 
