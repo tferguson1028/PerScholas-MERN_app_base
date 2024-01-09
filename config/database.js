1f2a19db39b3b0cf6118f9b636f8f85df5e96be3
Tesean Ferguson <tferguson1028@outlook.com>
2024-01-09 11:50:21 -0500

    Changed database location

--- config/database.js
+++ config/database.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.DATABASE_URI);
+mongoose.connect(process.env.DATABASE_URI, {dbName: "PerScholas"});
 
 const db = mongoose.connection;
 
