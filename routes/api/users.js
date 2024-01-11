dae7308c7c3cac53e5208a597a7eae192ed8c909
Tesean Ferguson <tferguson1028@outlook.com>
2024-01-11 13:57:38 -0500

    Added check to ensure correct user

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,12 +1,13 @@
 const express = require('express');
 const router = express.Router();
-const usersCtrl = require('../../controllers/api/users');
+const usersCtrl = require("../../controllers/api/users");
+const ensureLoggedIn = require("../../config/ensureLoggedIn");
 
 // POST /api/users
 router.post('/', usersCtrl.create);
 router.post("/login", usersCtrl.login);
 
 // GET /api/users
-router.get("/check-token", usersCtrl.checkToken);
+router.get("/check-token", ensureLoggedIn, usersCtrl.checkToken); // Insert ensureLoggedIn on all routes that need protecting
 
 module.exports = router;
